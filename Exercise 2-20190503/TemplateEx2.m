91df8a12dc6ade7ee62c9964d8af8d4b6f033b70
Takayuki Ota <takayukiota0905@gmail.com>
2019-05-05 19:21:21 +0200

    Finish ex2 task1.

--- Exercise 2-20190503/TemplateEx2.m
+++ Exercise 2-20190503/TemplateEx2.m
@@ -16,11 +16,35 @@ close all;
 % Task 1
 %--------------------------------------------------------------------------
 
-% Ho: 
-% Ha:      
+% Ho: x_bar = E(x)  
+% Ha: x_bar > E(x)     
 
+% expectation value: E(x)
+E = 0; % mm
 
+% mean
+x_bar = 3.5; % mm
 
+% std of mean
+s_x_bar = 1.7; % mm
+
+% Degree of Freedom
+f = 9; 
+
+% Test statistics of t-test
+T_t = (x_bar-E)/s_x_bar;
+
+% Threshold value of t-test
+t_t = tinv(0.95,9);
+
+% Statistical test for a = 0.05 S = 95%
+if T_t < t_t
+    disp('Fails to reject the Ho.')
+else
+    disp('Rejects the Ho.')
+end
+
+% [h,p,ci,stats] = ttest(E,x_bar);
 
 %--------------------------------------------------------------------------
 % Task 2
@@ -37,16 +61,16 @@ close all;
 
 % Ho: 
 % Ha: 
-
-s2_2
-s2_5
-
-f=
-
-Tf
-tf = finv(0.95,f,f);
-
-if Tf<tf
+% 
+% s2_2
+% s2_5
+% 
+% f=
+% 
+% Tf
+% tf = finv(0.95,f,f);
+% 
+% if Tf<tf
     
 
 
