8ad3608979df3938a9068f1b8ae4296d14a6e565
Takayuki Ota <takayukiota0905@gmail.com>
2019-05-13 11:38:59 +0200

    Finish ex3 task2.

--- Exercise 3-20190509/Ex3Task2.m
+++ Exercise 3-20190509/Ex3Task2.m
@@ -8,7 +8,6 @@
 %   Last changes   : Mar. 26, 2019
 %
 %--------------------------------------------------------------------------
-%  Task 1
 
 clc;
 clear all;
@@ -18,20 +17,20 @@ close all;
 % Calculate the rank of the design matrix A
 %--------------------------------------------------------------------------
 
-A=[-9 -10 -7 -1 -8;
-   -3 5 -5 8 3; 
-   8 0 8 -8 0;
-   -16 0 -10 16 6;
-   3 8 0 5 5;
-   -13 2 -7 15 8;
-   4 2 10 -2 8;
-   15 8 4 -7 -3;
-   2 6 0 4 4;
-   7 2 -1 -5 -6];
+A=[-6 -3 -1 2 1 -14;
+   -2 -8 -5 9 4 -13; 
+   7 5 -9 3 -6 11;
+   5 1 -3 7 4 3;
+   1 2 -6 8 2 -6;
+   8 6 -8 1 -7 15;
+   8 5 -5 2 -3 14;
+   7 9 -4 2 -2 12;];
+  
 
 r_A = rank(A);
 
-d = 5 - r_A; % rank deficiency = unknown - rank of design matrix
+d = 6 - r_A; % rank deficiency = unknown - rank of design matrix
+%  6 - 4 = 2
 %--------------------------------------------------------------------------
 % Calculate the normal matrix N and the determinant
 %--------------------------------------------------------------------------
@@ -46,32 +45,32 @@ Determinant of N = 0. That means normal matrix N is singular and
 %}
 
 r_N = rank(N); 
-% rank of N = 3. It's not maximal and rank deficiency is 2.
+% rank of N = 4. It's not maximal and rank deficiency is 2.
 
 %--------------------------------------------------------------------------
 % Calculate 3 different g-inverses of the matrix N
 %--------------------------------------------------------------------------
 %First g-inverse
-B1 = randi([-500 500],5,2); % two vectors with 5 random numbers from -500 to 500. 
+B1 = randi([-500 500],6,2); % two vectors with 5 random numbers from -500 to 500. 
 
 Next1 = [N B1; B1' zeros(2,2)];
 
 Next1_inv = inv(Next1);
 
-Q_11_1 = Next1_inv(1:5,1:5); % N^-
+Q_11_1 = Next1_inv(1:6,1:6); % N^-
 
 N1 = N*Q_11_1*N;
 
 t1 = trace(Q_11_1);
 
 %Second g-inverse
-B2 = randi([-100 100],5,2); % two vectors with 5 random number from -100 to 100. 
+B2 = randi([-100 100],6,2); % two vectors with 5 random number from -100 to 100. 
 
 Next2 = [N B2; B2' zeros(2,2)];
 
 Next2_inv = inv(Next2);
 
-Q_11_2 = Next2_inv(1:5,1:5);
+Q_11_2 = Next2_inv(1:6,1:6);
 
 N2 = N*Q_11_2*N;
 
@@ -79,13 +78,13 @@ t2 = trace(Q_11_2);
 
 
 %Third g-inverse
-B3 = randi([-300 300],5,2); % two vectors with 5 random number from -300 to 300. 
+B3 = randi([-300 300],6,2); % two vectors with 5 random number from -300 to 300. 
 
 Next3 = [N B3; B3' zeros(2,2)];
 
 Next3_inv = inv(Next3);
 
-Q_11_3 = Next3_inv(1:5,1:5);
+Q_11_3 = Next3_inv(1:6,1:6);
 
 N3 = N*Q_11_3*N;
 
@@ -115,7 +114,7 @@ Next_v = [N vv; vv' zeros(2,2)];
 
 Next_v_inv = inv(Next_v);
 
-Q_11_4 = Next_v_inv(1:5,1:5);
+Q_11_4 = Next_v_inv(1:6,1:6);
 
 N4 = N*Q_11_4*N;
 
